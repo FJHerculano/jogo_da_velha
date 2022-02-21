4c2bb8f29fa55a26b68e3cd6dae98a4ee8ab92eb
fjcoherculano <franciscoherculano.dev@incibrasil.com.br>
2022-02-21 01:40:05 -0300

    Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let buttons = document.querySelectorAll("#butons-container button");
-let messageContainer = document.querySelector("message");
-let messageText = document.querySelector("message p");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
 let secondPlayer;
 
 // contador de jogadas 
@@ -27,6 +27,13 @@ for(let i = 0; i < boxes.length; i++){
             // Computando jogada
             if(player1 == player2){
                 player1++;
+
+                if(secondPlayer == 'ia-player'){
+                    // função executar jogada
+                    computerPlay();
+                    player2++;
+                }
+
             } else {
                 player2++;
             }
@@ -38,6 +45,24 @@ for(let i = 0; i < boxes.length; i++){
 
     });
 }
+
+//Eventos para saber se é 2 player ou IA
+for (let i = 0; i<buttons.length; i++){
+    
+    buttons[i].addEventListener("click", function(){
+        secondPlayer = this.getAttribute("id");
+
+        for(let j = 0; j<buttons.length; j++){
+            buttons[j].style.display = 'none';
+        }
+
+        setTimeout(function(){
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+        }, 500);
+    });
+}
+
 
 // Verifica e atribui o elemento ao jogador
 function checkEl(player1, player2){
@@ -226,8 +251,51 @@ function declareWinner(winner){
         msg = "Deu velha!";
     }
 
+
     // exibe mensagem
     messageText.innerHTML = msg;
     messageContainer.classList.remove("hide");
 
+    // esconde msg
+    setTimeout(function(){
+        messageContainer.classList.add("hide");
+    }, 2000);
+
+    // Zera as jogadas
+    player1 = 0;
+    player2 = 0;
+
+    // remove x e o
+    let boxesToRemove = document.querySelectorAll(".box div");
+
+    for(let i=0; i< boxesToRemove.length; i++ ){
+        boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+}
+
+// executar a lógica da jogada do cpu
+function computerPlay(){
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i=0 ; i<boxes.length; i++){
+        let randomNumber = Math.floor(Math.random()*5);
+
+        // Só preencher se estiver vazio o filho
+        if(boxes[i].childNodes[0] == undefined){
+            if(randomNumber <= 1){
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+            // chegagem de quantas estão preenchidas
+        }else{
+            filled++;
+        }
+    }
+
+    if(counter == 0 && filled < 9 ){
+        computerPlay();
+    }
 }
